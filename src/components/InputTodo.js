34d66ff8978fac81112db3f0eb59219ba438624a
Tumworobere <annaht.annah@gmail.com>
2022-01-12 03:47:12 +0300

    fix linter errors

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-alert */
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React, { useState } from 'react';
 
 const InputTodo = (props) => {
